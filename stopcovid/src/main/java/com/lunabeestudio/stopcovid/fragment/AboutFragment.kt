f60d89698206ffc654012d1ae0ed382c0a30af55
ofa <ofa@github>
2020-07-09 18:25:00 +0200

    add version check and update in About

--- stopcovid/src/main/java/com/lunabeestudio/stopcovid/fragment/AboutFragment.kt
+++ stopcovid/src/main/java/com/lunabeestudio/stopcovid/fragment/AboutFragment.kt
@@ -11,19 +11,65 @@
 package com.lunabeestudio.stopcovid.fragment
 
 import android.view.Gravity
+import android.widget.Toast
 import com.lunabeestudio.stopcovid.BuildConfig
 import com.lunabeestudio.stopcovid.R
 import com.lunabeestudio.stopcovid.coreui.fastitem.captionItem
 import com.lunabeestudio.stopcovid.coreui.fastitem.dividerItem
 import com.lunabeestudio.stopcovid.coreui.fastitem.spaceItem
 import com.lunabeestudio.stopcovid.coreui.fastitem.titleItem
+import com.lunabeestudio.stopcovid.extension.openInChromeTab
 import com.lunabeestudio.stopcovid.fastitem.linkItem
 import com.lunabeestudio.stopcovid.fastitem.logoItem
 import com.mikepenz.fastadapter.GenericItem
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import okhttp3.OkHttpClient
+import okhttp3.Request
 
 class AboutFragment : MainFragment() {
 
     override fun getTitleKey(): String = "aboutController.title"
+
+    private val releasesUri = "https://github.com/ofa-/stopcovid-android/releases"
+    private val releaseApk = "stopcovid-release.apk"
+
+    private fun isLatest() : Boolean {
+        "$releasesUri/latest".run {
+            return (OkHttpClient.Builder().build()
+                .newCall(Request.Builder().url(this).build())
+                .execute()
+                .body?.string() ?: "")
+                .contains("commit/" + BuildConfig.BUILD_ID)
+        }
+    }
+
+    private fun toast(text: String) {
+        CoroutineScope(Dispatchers.Main).launch {
+            Toast.makeText(requireContext(), text, Toast.LENGTH_SHORT).also {
+                it.setGravity(Gravity.CENTER, 0, 0)
+            }.show()
+        }
+    }
+
+    private fun checkNewVersion() {
+        CoroutineScope(Dispatchers.Default).launch {
+            runCatching {
+                if (isLatest()) {
+                    toast("💕  🐭   already latest   🐱  💋")
+                } else {
+                    toast("⏬ 🐱   downloading latest   🐭 ⏬")
+                    delay(2000)
+                    "$releasesUri/latest/download/$releaseApk"
+                        .openInChromeTab(requireContext())
+                }
+            }.onFailure {
+                toast("🐰  error fetching updates  🎃")
+            }
+        }
+    }
 
     override fun getItems(): List<GenericItem> {
         val items = ArrayList<GenericItem>()
@@ -42,6 +88,13 @@ class AboutFragment : MainFragment() {
                 .plus("\n\n(" + BuildConfig.BUILD_ID + ")")
             gravity = Gravity.CENTER
             identifier = items.size.toLong()
+            ripple = true
+            onLongClick = {
+                releasesUri.openInChromeTab(requireContext())
+            }
+            onClick = {
+                checkNewVersion()
+            }
         }
         items += spaceItem {
             spaceRes = R.dimen.spacing_xlarge
