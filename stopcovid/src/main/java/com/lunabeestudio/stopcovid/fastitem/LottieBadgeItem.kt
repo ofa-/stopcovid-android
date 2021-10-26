e4228e55d957bab8e416404703d65b96faeae632
stopcovid@lunabee.com <StopCovid Kick-Ass>
2021-10-26 09:36:54 +0200

    Update to 3.8.4
    - Lottie on health screen

---
+++ stopcovid/src/main/java/com/lunabeestudio/stopcovid/fastitem/LottieBadgeItem.kt
@@ -0,0 +1,48 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ *
+ * Authors
+ * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ * Created by Lunabee Studio / Date - 2020/04/05 - for the TOUS-ANTI-COVID project
+ */
+
+package com.lunabeestudio.stopcovid.fastitem
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.annotation.RawRes
+import com.airbnb.lottie.LottieCompositionFactory
+import com.lunabeestudio.stopcovid.coreui.extension.setTextOrHide
+import com.lunabeestudio.stopcovid.databinding.ItemLottieBadgeBinding
+import com.lunabeestudio.stopcovid.databinding.NestedItemLottieBinding
+import com.mikepenz.fastadapter.binding.AbstractBindingItem
+
+class LottieBadgeItem(@RawRes private val rawRes: Int) : AbstractBindingItem<ItemLottieBadgeBinding>() {
+    override val type: Int = rawRes
+
+    var chipText: String? = null
+    var chipClick: (() -> Unit)? = null
+
+    override fun createBinding(inflater: LayoutInflater, parent: ViewGroup?): ItemLottieBadgeBinding {
+        val itemLottieBinding = ItemLottieBadgeBinding.inflate(inflater, parent, false)
+        val nestedItemLottieBadgeBinding = NestedItemLottieBinding.bind(itemLottieBinding.root)
+        LottieCompositionFactory.fromRawRes(inflater.context, rawRes).addListener { composition ->
+            nestedItemLottieBadgeBinding.lottieAnimationView.setComposition(composition)
+        }
+        return itemLottieBinding
+    }
+
+    override fun bindView(binding: ItemLottieBadgeBinding, payloads: List<Any>) {
+        super.bindView(binding, payloads)
+        binding.timeChip.setTextOrHide(chipText)
+        val clickListener = chipClick?.let {
+            View.OnClickListener { it() }
+        }
+        binding.timeChip.setOnClickListener(clickListener)
+    }
+}
+
+fun lottieBadgeItem(@RawRes rawRes: Int, block: (LottieBadgeItem.() -> Unit)): LottieBadgeItem = LottieBadgeItem(rawRes).apply(block)
